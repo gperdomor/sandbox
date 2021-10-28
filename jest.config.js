699053f89c3d98170bd318f84a9eec7f64a55360
Gustavo Perdomo <gperdomor@gmail.com>
2021-10-28 20:28:33 -0300

    chore: update to nx 13 and nx-docker 2.0.0-alpha.1

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  projects: ['<rootDir>/apps/web', '<rootDir>/apps/api'],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
